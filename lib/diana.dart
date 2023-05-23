aee4bb7ad29ac50f0c2c5952b600971d67773517
diana <dianadibah11@gmail.com>
2023-05-23 21:14:14 +0300

    2

--- lib/diana.dart
+++ lib/diana.dart
@@ -1,5 +1,11 @@
+
 import 'package:flutter/material.dart';
 
-class MyColors{
-  Color black=Colors.black;
+class Diana extends StatelessWidget {
+  const Diana({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
 }
